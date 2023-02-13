9225e037db758b15c17815e86ef3cffcf9142238
Ali Kamal <alikamaltaki@gmail.com>
2023-02-13 14:59:18 +0300

    Update repository.dart

--- lib/repository.dart
+++ lib/repository.dart
@@ -1,25 +1,26 @@
 import 'dart:io';
-import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+import 'package:flutter/foundation.dart';
 
-class RemoteRepository{
+class RemoteRepository {
   const RemoteRepository();
 
   Future postHttp(String message) async {
     Dio dio = Dio();
-    (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
-        (HttpClient client) {
-      client.badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-      return client;
-    };
-    try {
+    if (!kIsWeb) {
+      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+          (HttpClient client) {
+        client.badCertificateCallback =
+            (X509Certificate cert, String host, int port) => true;
+        return client;
+      };
+    }
 
-      var response = await dio.post('https://testapi-0b4e.onrender.com/api_request',
-      queryParameters:{
-        "prompt":message
-      }
-      );
+    try {
+      var response = await dio.post(
+          'https://testapi-0b4e.onrender.com/api_request',
+          queryParameters: {"prompt": message});
       print(response);
       return response;
     } catch (e) {
